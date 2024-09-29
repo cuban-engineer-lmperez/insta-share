bb5be2da6cc98329468c1c4fd42f337454b57e05
Luis Manuel Pérez Batista <luismanuelp1992@gmail.com>
2024-09-29 12:35:59 +0000

    feat: add create account form validations

--- src/app/pages/components/register/register.component.ts
+++ src/app/pages/components/register/register.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
@@ -17,28 +17,65 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-  hide = true;
+  hide = signal(true);
+  confirmHide = signal(true);
   registerForm: FormGroup;
-  incorrectCredentials = false;
+  incorrectCredentials = signal(false);
+  passwordsDontMatch = signal(false);
 
   constructor() {
     this.registerForm = new FormGroup({
       username: new FormControl('', {
-        updateOn: 'submit',
-        validators: [Validators.required]
+        updateOn: 'change',
+        validators: [Validators.required, Validators.email]
       }),
       password: new FormControl('', {
-        updateOn: 'submit',
+        updateOn: 'change',
         validators: [Validators.required]
-      })
-    })
+      }),
+      confirmPassword: new FormControl('', {
+        updateOn: 'change',
+        validators: [Validators.required]
+      }),
+    },
+      { validators: this.checkMatchingPassword }
+    )
   }
 
   get f(): { [key: string]: AbstractControl } {
     return this.registerForm.controls;
   }
 
+  /**
+   * Create a new account with the register form data
+   */
   register() {
-    // Implement
+    if (this.registerForm.valid) {
+      alert('register')
+    }
   }
+
+  checkMatchingPassword: ValidatorFn = (
+    control: AbstractControl,
+  ): ValidationErrors | null => {
+    let password = control.get('password');
+    let confirmPassword = control.get('confirmPassword');
+    const passNotMatch = password && confirmPassword && password.value !== confirmPassword.value;
+    let passwordErrors = password?.errors || null;
+    let confirmPasswordErrors = password?.errors || null;
+    if (!!passNotMatch) {
+      password?.setErrors({ ...passwordErrors, passwordNoMatch: true });
+      confirmPassword?.setErrors({ ...confirmPasswordErrors, passwordNoMatch: true });
+    } else {
+      delete passwordErrors?.['passwordNoMatch'];
+      if(passwordErrors && Object.keys(passwordErrors as Object).length === 0)  passwordErrors = null
+      delete confirmPasswordErrors?.['passwordNoMatch'];
+      if(confirmPasswordErrors && Object.keys(confirmPasswordErrors as Object).length === 0)  confirmPasswordErrors = null
+      password?.setErrors(passwordErrors);
+      confirmPassword?.setErrors(confirmPasswordErrors);
+    }
+    return !!passNotMatch ? { passwordNoMatch: true } : null;
+  };
+
 }
+
