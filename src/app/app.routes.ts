632e7e56f22f588e18c27f65194f7b1b6526c1fb
Luis Manuel Pérez Batista <luismanuelp1992@gmail.com>
2024-09-29 13:43:36 +0000

    fix: reroute register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { RegisterComponent } from './pages/components/register/register.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     { path: 'register', component: RegisterComponent },
